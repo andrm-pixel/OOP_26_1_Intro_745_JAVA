ca21d5f7fef1abcd2ac871612118bfca6896c979
unknown <andrmerlano@gmail.com>
2026-02-27 16:55:09 -0500

    Implementar mayorDeTresNumeros

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -383,7 +383,7 @@ Rock crushes Scissors
     }
 
     public double areaCirculo(double radio) {
-        return (Math.PI * (radio));
+        return (Math.PI * (radio*radio));
     }
 
     public String zoodiac(int day, int month) {
